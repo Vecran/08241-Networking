6f8ae3cb8f9ca41323ac7c6214c75361347d0b92
Alex Fisher <fisheralex92@hotmail.com>
2016-03-09 14:07:32 +0000

    Location Client and Server

---
+++ location/location/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+/// <summary>
+/// Location
+/// </summary>
+public class Connect
+{
+    static string Hosting(string[] args)
+    {
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "-h")
+            {
+                return args[i + 1];
+            }
+        }
+        return "localhost";
+    }
+    static int RequestFormat(string[] args)
+    {
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "-h9")
+            {
+                return 1;
+            }
+            else if (args[i] == "-h0")
+            {
+                return 2;
+            }
+            else if (args[i] == "h1")
+            {
+                return 3;
+            }
+        }
+        return 0;
+    }
+    static string Formatting(int format, string[] args, string host)
+    {
+        //whois style requests
+        if (format == 0)
+        {
+            if (args.Length == 3 || args.Length == 1)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i] != host && args[i] != "-h")
+                    {
+                        return args[i];
+                    }
+                }
+                return "FALSE";
+            }
+            else if (args.Length == 2 || args.Length == 4)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i] != host && args[i] != "-h")
+                    {
+                        return (args[i] + " " + args[i + 1]);
+                    }
+                }
+                return "FALSE";
+            }
+            else return "FALSE";
+        }
+        else if (format == 1) // http 0.9 style requests
+        {
+            if (args.Length == 2 || args.Length == 4)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i] != host && args[i] != "-h" && args[i] != "-h9")
+                    {
+                        return "GET /" + args[i];
+                    }
+                }
+                return "FALSE";
+            }
+            else if (args.Length == 3 || args.Length == 5)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i] != host && args[i] != "-h" && args[i] != "-h9")
+                    {
+                        return ("PUT /" + args[i] + "\r\n\r\n" + args[i + 1]);
+                    }
+                }
+                return "FALSE";
+            }
+            else return "FALSE";
+        }
+        else if (format == 2) // http 1.0
+        {
+            if (args.Length == 2 || args.Length == 4)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i] != host && args[i] != "-h" && args[i] != "-h0")
+                    {
+                        return args[i];
+                    }
+                }
+                return "FALSE";
+            }
+            else if (args.Length == 3 || args.Length == 5)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i] != host && args[i] != "-h" && args[i] != "-h0")
+                    {
+                        return (args[i] + " " + args[i + 1]);
+                    }
+                }
+                return "FALSE";
+            }
+            else return "FALSE";
+        }
+        else if (format == 3) //http 1.1
+        {
+            if (args.Length == 2 || args.Length == 4)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i] != host && args[i] != "-h" && args[i] != "-h1")
+                    {
+                        return args[i];
+                    }
+                }
+                return "FALSE";
+            }
+            else if (args.Length == 3 || args.Length == 5)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i] != host && args[i] != "-h" && args[i] != "-h1")
+                    {
+                        return (args[i] + " " + args[i + 1]);
+                    }
+                }
+                return "FALSE";
+            }
+            else return "FALSE";
+        }
+
+        else return "FALSE";
+    }
+    /// <summary>
+    /// Main program
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        try
+        {
+            //opens connection to server on localhost at port 43
+            //sets up readers and writers
+            TcpClient client = new TcpClient();
+            string host = Hosting(args);
+            client.Connect(host, 43);
+
+            //client.ReceiveTimeout = 1000;
+            //client.SendTimeout = 1000;
+            StreamWriter sw = new StreamWriter(client.GetStream());
+            StreamReader sr = new StreamReader(client.GetStream());
+            sw.WriteLine(Formatting(RequestFormat(args), args, host));
+            //flush server commands and read reply
+            sw.Flush();
+            sw.Close();
+            string response = sr.ReadToEnd().Trim();
+
+            //outputs reply to console
+            if (response == " ")
+            {
+                Console.WriteLine("No Response");
+            }
+            else if (response == "OK")
+            {
+                Console.WriteLine(response);
+            }
+            else
+            {
+                Console.WriteLine(response);
+            }
+        }
+        catch
+        {
+            Console.Write("ya fucked up");
+            Console.Write("ERROR: no entries found \r\n");
+        }
+    }
+}
+
---
+++ locationserver/locationserver/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+/// <summary>
+/// LocationServer
+/// </summary>
+public class Respond
+{
+
+    static Dictionary<string, string> dictionary = new Dictionary<string, string>()
+    {
+        {"470566", "Fenner-052"}
+    };
+
+    /// <summary>
+    /// main takes in array of strings from command line
+    /// </summary>
+    /// <param name="args"></param>
+    public static void Main(string[] args)
+    {
+        runServer();
+    }
+    /// <summary>
+    /// called by the main, creates and runs the server
+    /// </summary>
+    public static void runServer()
+    {
+        TcpListener listener;
+        Socket connection;
+        NetworkStream socketStream;
+        try
+        {
+            //creates tcp socket which listens on port 43 for incoming requests
+            listener = new TcpListener(IPAddress.Any, 43);
+            listener.Start();
+            Console.WriteLine("Server started listening");
+
+            //loop forever
+            //handles requests
+            while (true)
+            {
+                //create a socket to handle a recieved request
+                connection = listener.AcceptSocket();
+                socketStream = new NetworkStream(connection);
+                Console.WriteLine("Connection Recieved");
+                doRequest(socketStream);
+
+                //close sockets and connection once request is done
+                socketStream.Close();
+                connection.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception" + e.ToString());
+        }
+    }
+    /// <summary>
+    /// called after the server recieves a connection on a listener
+    /// </summary>
+    /// <param name="socketStream"></param>
+    public static void doRequest(NetworkStream socketStream)
+    {
+        try
+        {
+            //socketStream.ReadTimeout = 1000;
+            //socketStream.WriteTimeout = 1000;
+
+            //creates stream reader/writer for socket I/O
+            StreamWriter sw = new StreamWriter(socketStream);
+            StreamReader sr = new StreamReader(socketStream);
+
+            //reading first line of request, trims edges to remove spaces and \n \r
+            string line = sr.ReadToEnd();
+            Console.WriteLine("LocationServer Received " + line);
+
+            //splits line at first space character into 2 parts
+            string[] sections = line.Split(new char[] { ' ' }, 2);
+            if (sections.Length == 1)
+            {
+
+                if (dictionary.ContainsKey(sections[0]))
+                {
+                    string location = dictionary[sections[0]];
+                    sw.WriteLine(location);
+                    sw.Flush();
+                }
+                else
+                {
+                    sw.WriteLine("ERROR: no entries found");
+                    sw.Flush();
+                }
+            }
+            else if (sections.Length == 2)
+            {
+                sw.WriteLine("OK");
+                sw.Flush();
+                if (dictionary.ContainsKey(sections[0]))
+                {
+                    dictionary[sections[0]] = sections[1];
+                }
+                else if (!dictionary.ContainsKey(sections[0]))
+                {
+                    dictionary.Add(sections[0], sections[1]);
+                }
+            }
+
+
+
+        }
+        catch
+        {
+            //catch in case somehting goes really wrong
+            Console.WriteLine("Unrecognised Command \nConnection Broken");
+        }
+    }
+}
+
